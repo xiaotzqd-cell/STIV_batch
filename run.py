cddaf5883866e717e7b6cf8136f4e3b29f681ad2
xiaotzqd-cell <xiaotzqd@gmail.com>
2025-11-19 14:42:30 +0800

    修改了测速线长度

--- run.py
+++ run.py
@@ -7,21 +7,21 @@ from stiv_adapt.search import adaptive_direction_search
 from stiv_adapt.core import init_debug_dir
 # ========== 用户配置区（按需修改） ==========
 VIDEO = r"D:\Programs\Python\stiv1\CRR.MP4"
-CENTER: Tuple[int, int] =(1870, 1117)  # ← 手动中心点（像素坐标）
+CENTER: Tuple[int, int] =(1987, 570)#(1870, 1117)  # ← 手动中心点（像素坐标）
 
 #多点测速参数
-USE_BATCH_LINE_PROBING = True # ← 开启多点测速
-BANK_POINT: Tuple[int, int] = (533, 1120) # 岸边点（与 CENTER 组成测速直线）
+USE_BATCH_LINE_PROBING = False # ← 开启多点测速
+BANK_POINT: Tuple[int, int] = (783, 577)#(533, 1120) # 岸边点（与 CENTER 组成测速直线）
 PROBE_INTERVAL_PX = 150 # 两测点之间的像素间隔（从中心点向两端延伸）
 # STI 测线参数（角度搜索范围：线方向）
-LENGTH_PX = 300
-ANGLE_START, ANGLE_END, ANGLE_STEP = -90, -86, 1   # 遍历的“测速线角度”
-MAX_FRAMES = 300
+LENGTH_PX = 256
+ANGLE_START, ANGLE_END, ANGLE_STEP = -92, -88, 1   # 遍历的“测速线角度”
+MAX_FRAMES = 256
 USE_ROI = True
 VERBOSE = True
 
 # 频域扇形增强（用于评分）
-USE_FFT_FAN = True
+USE_FFT_FAN = False
 FFT_HALF_DEG = 4
 FFT_RMIN_RATIO = 0.15
 FFT_RMAX_RATIO = 0.9
@@ -30,10 +30,10 @@ FPS: Optional[float] = 23.976
 # 比例尺：二选一
 SCALE_M_PER_PIXEL: Optional[float] = None  # A) 直接给（m/px）；不想手填则设 None 走 B)
 CALIB_REAL_M: Optional[float] = 49.38      # B) 首帧两点标定（米）
-CALIB_LINE_XYXY: Optional[Tuple[int, int, int, int]] = (445, 1321, 3080, 1439)
+CALIB_LINE_XYXY: Optional[Tuple[int, int, int, int]] = (445, 1321, 3085, 1444)
 #投票霍夫的可调参数（法线角 θ 的设置）——
 VOTE_THETA_RES_DEG = 1                 # 角度分辨率（度）
-VOTE_K_RATIO: float = 0.52             # 用比例阈值 K=0.55*R
+VOTE_K_RATIO: float = 0.55             # 用比例阈值 K=0.55*R
 VOTE_THETA_RANGE = (0.0, 180.0)        # 有效法线角范围 [min, max)
 # ==========================================
 def compute_scale_from_first_frame(video_path: str,
